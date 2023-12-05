ebd8de3b382dfc8dd20504eb22aa83fb1eeb4a90
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-12-05 18:50:36 +0530

    Added: UC2 check valid last name

--- userregister/UserRegisteration.java
+++ userregister/UserRegisteration.java
@@ -8,10 +8,17 @@ public class UserRegisteration {
         System.out.println("Enter First Name:");
         String fname = sc.next();
         System.out.println(checkValidFname(fname));
+        System.out.println("Enter Last Name");
+        String lname = sc.next();
+        System.out.println(checkValidLname(lname));
     }
 
     public static boolean checkValidFname(String fname) {
         String patt = "^(Cap)([a-z]|[A-Z]){3,}";
         return fname.matches(patt);
+    }
+    public static boolean checkValidLname(String lname){
+        String patt = "^(Cap)([a-z]|[A-Z]){3,}";
+        return lname.matches(patt);
     }
 }
