06f8dfc6584df5227662a04c72be42b27774b61d
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-12-05 18:45:03 +0530

    Added: UC1 check valid first name

--- userregister/UserRegisteration.java
+++ userregister/UserRegisteration.java
@@ -1,7 +1,17 @@
 package userregister;
 
+import java.util.Scanner;
+
 public class UserRegisteration {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name:");
+        String fname = sc.next();
+        System.out.println(checkValidFname(fname));
+    }
 
+    private static boolean checkValidFname(String fname) {
+        String patt = "^(Cap)([a-z]|[A-Z]){3,}";
+        return fname.matches(patt);
     }
 }
